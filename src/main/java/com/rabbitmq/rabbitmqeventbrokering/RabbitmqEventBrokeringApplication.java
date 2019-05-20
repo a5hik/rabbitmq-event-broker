31b219d3621838bfc9940821ba1005171094c81e
ashik <ashikes@gmail.com>
2019-05-20 19:32:57 +0530

    Publish msg.

--- src/main/java/com/rabbitmq/rabbitmqeventbrokering/RabbitmqEventBrokeringApplication.java
+++ src/main/java/com/rabbitmq/rabbitmqeventbrokering/RabbitmqEventBrokeringApplication.java
@@ -1,13 +1,27 @@
 package com.rabbitmq.rabbitmqeventbrokering;
 
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RabbitmqEventBrokeringApplication {
+public class RabbitmqEventBrokeringApplication implements CommandLineRunner {
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
 
     public static void main(String[] args) {
         SpringApplication.run(RabbitmqEventBrokeringApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+
+        SimpleMessage message = new SimpleMessage();
+        message.setName("Simple message");
+        message.setDescription("Simple Description");
+        rabbitTemplate.convertAndSend("TestExchange", "testRouting", message);
+    }
 }
